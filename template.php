8f8c14bf6448d1e9e916db0acd92b8de2b61cb7f
seutje <seutje@gmail.com>
2012-10-29 16:52:59 +0100

    Moved breakpoints order and added some header stuffs.

--- template.php
+++ template.php
@@ -3,4 +3,6 @@
  * @file
  * Provides overrides and additions to aid the theme.
  */
- 
+function wundertheme_form_search_block_form_alter(&$form, &$form_state, $form_id) {
+  $form['search_block_form']['#attributes']['placeholder'] = t('Search...');
+}
