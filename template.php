6e3f6ee5a40b13877b0ac46d84dd899faa7d08b0
seutje <seutje@gmail.com>
2012-10-30 11:16:46 +0100

    Add support for threecol_25_50_25_stacked.

--- template.php
+++ template.php
@@ -20,7 +20,7 @@ function wundertheme_form_search_block_form_alter(&$form, &$form_state, $form_id
  */
 function wundertheme_css_alter(&$css) {
   foreach ($css as $path => $meta) {
-    if (strpos($path, 'threecol_33_34_33_stacked') !== FALSE) {
+    if (strpos($path, 'threecol_33_34_33_stacked') !== FALSE || strpos($path, 'threecol_25_50_25_stacked') !== FALSE) {
       unset($css[$path]);
     }
   }
