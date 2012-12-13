18431b2089f622da1566741b7ef094f9ec952fe9
frankbaele <frank.baele@wunderkraut.com>
2012-12-13 16:53:49 +0100

    add new ie solution

--- template.php
+++ template.php
@@ -25,3 +25,41 @@ function wundertheme_css_alter(&$css) {
     }
   }
 }
+
+/**
+ * Override or insert variables into the html template.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered. This is usually "html", but can
+ *   also be "maintenance_page" since zen_preprocess_maintenance_page() calls
+ *   this function to have consistent variables.
+ */
+function wundertheme_preprocess_html(&$variables, $hook) {
+
+  $theme_path = drupal_get_path('theme', 'wundertheme');
+
+  drupal_add_css( $theme_path . '/stylesheets/ie.css',
+    array(
+      'group' => CSS_THEME,
+      'browsers' => array(
+        'IE' => 'lt IE 9',
+        '!IE' => FALSE,
+      ),
+      'weight' => 999,
+      'every_page' => TRUE,
+    )
+  );
+  drupal_add_css( $theme_path . '/stylesheets/style.css',
+    array(
+      'group' => CSS_THEME,
+      'browsers' => array(
+        'IE' => 'gt IE 8',
+        '!IE' => TRUE,
+      ),
+      'weight' => 999,
+      'every_page' => TRUE,
+    )
+  );
+}
